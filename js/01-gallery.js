bd5fb9853c541f5a74a34e8d95224238b28af789
Anatoliy7777 <fullstackwebdevelopersodessa@gmail.com>
2022-02-11 14:25:32 +0200

    add gallery css

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const listContainer = document.querySelector(".gallery");
+const photoMarkup = createPhotoCard(galleryItems);
+
+listContainer.insertAdjacentHTML("beforeend", photoMarkup);
+listContainer.addEventListener("click", listContainerClick);
+
+function createPhotoCard(photos) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"/>
+                </a>
+            </div>
+        `;
+    })
+    .join("");
+}
+
+const instance = basicLightbox.create(`<img class="modal-img" src="">`, {
+  onShow: (instance) => {
+    window.addEventListener("keydown", closeModal);
+  },
+
+  onClose: (instance) => {
+    window.removeEventListener("keydown", closeModal);
+  },
+});
+
+function listContainerClick(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  event.preventDefault();
+
+  instance.element().querySelector("img").src = event.target.dataset.source;
+
+  instance.show();
+}
+
+function closeModal(e) {
+  if (e.key === "Escape") {
+    instance.close();
+    return;
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const listContainer = document.querySelector(".gallery");
+
+const createPhotoCard = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+            </a>
+        `;
+  })
+  .join("");
+
+listContainer.insertAdjacentHTML("beforeend", createPhotoCard);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
 
 console.log(galleryItems);
